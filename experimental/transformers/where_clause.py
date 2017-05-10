2e352f3bc37117966ffe2d26178214f52d2948b4
André Roberge <andre.roberge@gmail.com>
2017-05-10 07:14:24 -0300

    attempting to fix emoji for github

--- experimental/transformers/where_clause.py
+++ experimental/transformers/where_clause.py
@@ -5,7 +5,7 @@ block that would be ignored by Python. The idea was to use this as
 a _pythonic_ notation as an alternative for the optional type hinting described
 in PEP484.  **This idea has been rejected.**
 
-:Warning: This transformation **cannot** be used in the console.
+:warning: This transformation **cannot** be used in the console.
 
 For more details, please see two of my recent blog posts:
 
